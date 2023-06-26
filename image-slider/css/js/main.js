53d98eeb80b65bbf650ec53e65ca1d20a71a21db
arifkratoss <arifkratos21@gmail.com>
2023-06-26 16:24:00 +0700

    membuat repo baru untuk pembelajaran slider

---
+++ image-slider/css/js/main.js
@@ -0,0 +1,41 @@
+const slider = document.querySelector('.slider');
+const buttons = document.querySelectorAll('.btn');
+const options = document.querySelectorAll('.option');
+const slides = document.querySelectorAll('.img');
+
+let index = 1;
+let size = slides[index].clientWidth;
+
+update();
+
+function update() {
+  slider.style.transform = `translateX(${-size * index}px)`;
+}
+
+function slide() {
+  slider.style.transition = "transform .5s ease-in-out";
+  update();
+}
+
+function btnCheck() {
+  if (this.id === "prev") {
+    index--;
+  } else if (this.id === "next") {
+    index++;
+  }
+  slide();
+}
+slider.addEventListener('transitionend', () => {
+    if (slides[index].id === "last") {
+      slider.style.transition = "none";
+      index = slides.length - 2;
+      slider.style.transform = `translateX(${-size * index}px)`;
+    } else if (slides[index].id === "first") {
+      slider.style.transition = "none";
+      index = 1;
+      slider.style.transform = `translateX(${-size * index}px)`;
+    }
+  });
+  
+
+buttons.forEach(btn => btn.addEventListener('click', btnCheck));
